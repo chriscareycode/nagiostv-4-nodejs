2189bef03383e69122c071ea77351c7c2cfe79de
Chris Carey <chris.carey@gmail.com>
2017-08-06 08:32:22 -0700

    animate flynn

--- app/pods/components/flynn-css/component.js
+++ app/pods/components/flynn-css/component.js
@@ -12,25 +12,51 @@ export default Ember.Component.extend({
 
   howManyDown: 0,
 
-  // didInsertElement: function() {
-  // },
+  timerHandle: null,
+  timerFired: 0,
+
+  didInsertElement: function() {
+    const timerHandle = setInterval(() => {
+      this.set('timerFired', new Date().getTime());
+    }, 2000);
+    Ember.run.next(() => {
+      this.set('timerHandle', timerHandle);
+    });
+  },
+
+  willDestroyElement: function() {
+    const timerHandle = this.get('timerHandle');
+    if (timerHandle) {
+      clearInterval(timerHandle);
+    }
+  },
 
   // howManyDownWatcher: Ember.observer('howManyDown', function() {
   //   const howManyDown = this.get('howManyDown');
   //   console.log('howManyDown: ' + howManyDown);
   // }),
 
-  flynnClass: Ember.computed('howManyDown', function() {
+  happyClasses: ['flynn1', 'flynn6', 'flynn11'],
+  angryClasses: ['flynn2', 'flynn3', 'flynn7', 'flynn8', 'flynn12', 'flynn13', 'flynn16', 'flynn17', 'flynn18'],
+  bloodyClasses: ['flynn4', 'flynn5', 'flynn9', 'flynn10', 'flynn14', 'flynn15', 'flynn19', 'flynn24', 'flynn25'],
+
+  flynnClass: Ember.computed('howManyDown', 'timerFired', function() {
     const howManyDown = this.get('howManyDown');
     console.log('howManyDown: ' + howManyDown);
 
     let flynnClass;
     if (howManyDown === 0) {
-      flynnClass = 'flynn flynn1';
+      const happyClasses = this.get('happyClasses');
+      const item = happyClasses[Math.floor(Math.random()*happyClasses.length)];
+      flynnClass = 'flynn ' + item;
     } else if (howManyDown < 4) {
-      flynnClass = 'flynn flynn2';
+      const angryClasses = this.get('angryClasses');
+      const item = angryClasses[Math.floor(Math.random()*angryClasses.length)];
+      flynnClass = 'flynn ' + item;
     } else {
-      flynnClass = 'flynn flynn24';
+      const bloodyClasses = this.get('bloodyClasses');
+      const item = bloodyClasses[Math.floor(Math.random()*bloodyClasses.length)];
+      flynnClass = 'flynn ' + item;
     }
     return flynnClass;
   })
