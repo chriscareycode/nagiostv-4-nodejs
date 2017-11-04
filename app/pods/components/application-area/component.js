6c42fbbad3184ff0d489af1a3ca21eb2a2995b3e
Chris Carey <chris.carey@gmail.com>
2017-11-03 20:22:52 -0700

    Fix bug where All x services are UP was still showing when services were down.

--- app/pods/components/application-area/component.js
+++ app/pods/components/application-area/component.js
@@ -53,22 +53,8 @@ export default Ember.Component.extend({
     const dateLastUpdate = this.get('nagios.dateLastUpdate');
     const dateDrift = new Date().getTime() - dateLastUpdate;
     this.set('dateDrift', dateDrift);
-  },
+  }
 
-  atLeastOneServiceDownOrFlapping: Ember.computed('nagios.dateLastUpdate', function() {
-    let servicelist = this.get('nagios.servicelist');
-    let onedown = false;
-    //console.log('servicelist', servicelist);
-    for (let hostkey in servicelist) {
-      //console.log('each host ', servicelist[hostkey]);
-      for (let servicekey in servicelist[hostkey]) {
-        //console.log('each service ', servicelist[hostkey][servicekey]);
-        if (servicelist[hostkey][servicekey].is_flapping || servicelist[hostkey][servicekey].status !== 2) {
-          onedown = true;
-        }
-      }
-    }
-    return onedown;
-  })
+
 
 });
---
+++ app/pods/components/service-list/component.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  atLeastOneServiceDownOrFlapping: Ember.computed('nagios.dateLastUpdate', function() {
+    let servicelist = this.get('nagios.servicelist');
+    let onedown = false;
+    //console.log('servicelist', servicelist);
+    for (let hostkey in servicelist) {
+      //console.log('each host ', servicelist[hostkey]);
+      for (let servicekey in servicelist[hostkey]) {
+        //console.log('each service ', servicelist[hostkey][servicekey]);
+        if (servicelist[hostkey][servicekey].is_flapping || servicelist[hostkey][servicekey].status !== 2) {
+          onedown = true;
+        }
+      }
+    }
+    return onedown;
+  })
+
+});
