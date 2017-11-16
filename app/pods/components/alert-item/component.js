53496080ddb81bc310c22a656f74b9b370b32eee
Chris Carey <chris.carey@gmail.com>
2017-11-15 22:52:29 -0800

    Update some state and state class friendly text for alert-item component.

--- app/pods/components/alert-item/component.js
+++ app/pods/components/alert-item/component.js
@@ -24,7 +24,54 @@ export default Ember.Component.extend({
       case 32: // service critical
         classString = 'color-red';
         break;
+      case 64: // service unknown
+        classString = 'color-orange';
+        break;
     }
+    return classString;
+  }),
+
+  stateString: Ember.computed('item.state', function() {
+    const state = this.get('item.state');
+    const object_type = this.get('item.object_type');
+
+    let classString = '';
+
+    if (object_type === 1) {
+      // Host
+      switch(state) {
+        case 1:
+          classString = 'OK';
+          break;
+        case 2:
+          classString = 'CRITICAL';
+          break;
+        default:
+          classString = 'unknown ' + state;
+          break;
+      }
+    } else if (object_type === 2) {
+      // Service
+      switch(state) {
+        case 8:
+          classString = 'OK';
+          break;
+        case 16:
+          classString = 'WARNING';
+          break;
+        case 32:
+          classString = 'CRITICAL';
+          break;
+        case 64:
+          classString = 'UNKNOWN';
+          break;
+        default:
+          classString = 'unknown ' + state;
+          break;
+      }
+    }
+      
+
     return classString;
   }),
 
