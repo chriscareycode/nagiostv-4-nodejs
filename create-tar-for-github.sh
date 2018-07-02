05d3c3c9cc57f1328b0270f57d332ddc4d2d3a30
Chris Carey <chris.carey@gmail.com>
2018-07-01 19:46:39 -0700

    Delete the correct release file

--- create-tar-for-github.sh
+++ create-tar-for-github.sh
@@ -1,3 +1,3 @@
-rm nagiostv-1.0.0.tar.gz
+rm releases/nagiostv-1.0.0.tar.gz
 tar --exclude='./node/settings.js' --exclude='./node/settings-json.js' --exclude='./node/node_modules' --exclude='./node/.gitignore' -zcvf releases/nagiostv-1.0.0.tar.gz dist node
 echo Now rename releases/nagiostv-1.0.0.tar.gz to the new version name
